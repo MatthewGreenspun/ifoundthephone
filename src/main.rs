5c86eec0b395fae6f3beb5b645235afeb31241bb
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-11-12 15:32:36 -0500

    load env vars in main fn

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate rocket;
 use rocket_dyn_templates::Template;
 use rocket::fs::{FileServer, relative};
 mod routes;
+use routes::*;
 
 #[launch]
 fn rocket() -> _ {
+    dotenv::dotenv().expect("failed to load environment variables");
     rocket::build()
         .mount("/static", FileServer::from(relative!("/static")))
-        .mount("/", routes![routes::index, routes::sign_up_page, routes::sign_up, routes::login, routes::login_page])
+        .mount("/", routes![index, sign_up, sign_up_page, login, login_page, device_found_page])
         .attach(Template::fairing())
 }
