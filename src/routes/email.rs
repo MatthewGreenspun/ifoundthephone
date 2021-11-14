1be34a15fd00e802a35bb85b3ac8c61bc304430a
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-11-13 22:08:10 -0500

    added function to email device owner when their device is found

--- src/routes/email.rs
+++ src/routes/email.rs
@@ -1,5 +1,6 @@
 use lettre::transport::smtp::authentication::Credentials;
-use lettre::{Message, SmtpTransport, Transport};
+use lettre::{Message, SmtpTransport, Transport, message::header::ContentType};
+use super::form_structs::DeviceFoundRequest;
 
 pub fn email_new_user(user_email: &str) {
 	let ifoundthephone_email = std::env::var("EMAIL").expect("EMAIL environment variable not found");
@@ -12,6 +13,41 @@ pub fn email_new_user(user_email: &str) {
 		.reply_to(format!("ifoundthephone <{}>", &ifoundthephone_email).parse().unwrap())
 		.to(format!("<{}>", user_email).parse().unwrap())
 		.subject("Welcome to IFoundThePhone!")
+		.body(body)
+		.unwrap();
+
+	let mailer = SmtpTransport::relay("smtp.gmail.com")
+		.unwrap()
+		.credentials(creds)
+		.build();
+	
+	match mailer.send(&email) {
+			Ok(_) => println!("Email sent successfully!"),
+			Err(e) => println!("Could not send email: {:?}", e),
+	}
+}
+
+pub fn email_device_owner(device_owner_email: &str, email_info: DeviceFoundRequest) {
+	let ifoundthephone_email = std::env::var("EMAIL").expect("EMAIL environment variable not found");
+	let ifoundthephone_password = std::env::var("EMAIL_PASSWORD").expect("EMAIL_PASSWORD environment variable not found");
+	let creds = Credentials::new(ifoundthephone_email.clone(), ifoundthephone_password);
+	let body = format!("<h1>Your Device Was Found!</h1>
+		<h2>Here is some information that was provided by the person who found your device which will hopefully help you retrieve it:</h2>
+		<h3>Finder Email: {}</h3>
+		<h3>Finder Phone Number: {}</h3>
+		<h3>Finder Message: </h3>
+		<p>{}</p>", 
+		if email_info.email.len() > 0 {email_info.email} else {"<b>Not Provided</b>".to_string()},
+		if email_info.phone_number.len() > 0 {email_info.phone_number} else {"<b>Not Provided</b>".to_string()},
+		email_info.message)
+		.to_string();
+
+	let email = Message::builder()
+		.from(format!("ifoundthephone <{}>", &ifoundthephone_email).parse().unwrap())
+		.reply_to(format!("ifoundthephone <{}>", &ifoundthephone_email).parse().unwrap())
+		.to(format!("<{}>", device_owner_email).parse().unwrap())
+		.subject("Your device was found!")
+		.header(ContentType::TEXT_HTML)
 		.body(body)
 		.unwrap();
 
