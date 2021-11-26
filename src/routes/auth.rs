c72fa08ecf5d1581877b9f01c5290cc84b3d812e
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-11-25 19:26:56 -0500

    added function to verify user by email and password

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,6 +1,8 @@
 use ring::digest::SHA512_OUTPUT_LEN;
 use ring::{pbkdf2, rand::SecureRandom};
 use std::num::NonZeroU32;
+use hex::{FromHex, ToHex};
+use super::db::{email_exists, get_hash_and_salt};
 
 pub fn gen_salt() -> [u8; SHA512_OUTPUT_LEN] {
     let mut salt = [0u8; SHA512_OUTPUT_LEN];
@@ -20,4 +22,22 @@ pub fn gen_hash(salt: &[u8], password: &String) -> [u8; SHA512_OUTPUT_LEN] {
         &mut hash,
     );
     hash
+}
+
+pub async fn user_is_valid(email: &String, password: &String) -> bool {
+    if !email_exists(email).await {
+        return false
+    }
+
+    let (true_hash, salt) = match get_hash_and_salt(email).await {
+        Ok(hash_and_salt) => hash_and_salt, 
+        Err(e) => {
+            eprintln!("error fetching hash and salt: {:?}", e);
+            return false
+        }
+    };
+    let salt_buf = <[u8; SHA512_OUTPUT_LEN]>::from_hex(salt).expect("decoding salt failed");
+    let hash = gen_hash(&salt_buf, password).encode_hex::<String>();
+
+    if true_hash == hash {true} else {false}
 }
