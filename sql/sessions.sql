3d059f92078a1d2a42d54ea88bd2d5d85578391c
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-11-26 10:15:36 -0500

    edit sql tables

--- sql/sessions.sql
+++ sql/sessions.sql
@@ -1,4 +1,5 @@
 CREATE TABLE sessions (
-	session_id					VARCHAR(10) NOT NULL UNIQUE,
-	expires							DATE
+	session_id		VARCHAR(10) NOT NULL UNIQUE,
+	user_id				VARCHAR(10) NOT NULL REFERENCES users(user_id),
+	expires				DATE NOT NULL
 )
