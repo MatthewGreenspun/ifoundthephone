bce4aa047653c6c18342938ac8cf92f7d40d27fc
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-11-26 13:05:40 -0500

    added authenticated user request guard

--- src/routes/route_structs.rs
+++ src/routes/route_structs.rs
@@ -1,4 +1,7 @@
 use rocket::form::FromForm;
+use rocket::request::{ Request, FromRequest, Outcome};
+use rocket::http::{Status};
+use super::db;
 
 #[derive(FromForm)]
 pub struct NewUserRequest {
@@ -20,4 +23,53 @@ pub struct DeviceFoundRequest {
     pub email: String,
     #[field(name = "phone-number")]
     pub phone_number: String,
+}
+
+pub struct AuthenticatedUser {
+    pub user_id: String, 
+    pub session_id: String, 
+    pub email: String,
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    CookieNotFound, 
+    InvalidSession,
+    DbClientNotFound,
+    DbQueryFailure
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthenticatedUser {
+    type Error = AuthError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, AuthError> {
+        let session_id = match req.cookies().get("session_id") {
+            Some(cookie) => cookie.value().to_string(),
+            None => return Outcome::Failure((Status::Unauthorized, AuthError::CookieNotFound))
+        };
+        let db_client = match req.rocket().state::<db::DbClient>(){
+            Some(client) => client,
+            None => return Outcome::Failure((Status::InternalServerError, AuthError::DbClientNotFound))
+        };
+        let user_id = match db::get_session_user(&db_client.client, &session_id).await {
+            Ok(user_id) => user_id,
+            Err(e) => match e {
+                db::AuthError::DbError(e) => {
+                    eprintln!("error retrieving user id from database: {}", e);
+                    return Outcome::Failure((Status::InternalServerError, AuthError::DbQueryFailure))
+                },
+                db::AuthError::SessionInvalidError => return Outcome::Failure((Status::Unauthorized, AuthError::InvalidSession))
+            }
+        };
+        let email = match db::get_email(&db_client.client, &user_id).await {
+            Ok(email) => email,
+            Err(e) => {
+                eprintln!("error retrieving email from database: {}", e);
+                return Outcome::Failure((Status::InternalServerError, AuthError::DbQueryFailure))
+            }
+        };
+
+        Outcome::Success(AuthenticatedUser {user_id, session_id, email})
+    }
 }
